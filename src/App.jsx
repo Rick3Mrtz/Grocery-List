9ad699132bdc70534332679b43e9f97670dd8f45
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-07-24 18:46:35 -0500

    corrected grocery input, temporary styling added, react icons used as buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import react from 'react'
 import './App.css'
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { VscClose } from 'react-icons/vsc'
 
 function App() {
 
@@ -7,7 +9,7 @@ function App() {
   return (
     <>
 
-      <h1 className='flex text-4xl justify-center mt-[6vh] mb-[3vh]'>
+      <h1 className='flex text-4xl font-bold justify-center mt-[6vh] mb-[3vh]'>
         Brick's Grocery List
       </h1>
 
@@ -18,33 +20,48 @@ function App() {
         <button className=' btn'>Add</button>
       </form>
 
-      <div className='grid grid-cols-2 gap-2 mt-10'>
-        {/* Left Column - Groceries to Buy */}
-        <div className='bg-gray-200 p-4 ml-4 h-fit'>
-          <h1 className='text-xl text-center'>Groceries we need</h1>
-          <div className='flex justify-center items-center'>
-
-          </div>
-          <ul className=' mt-10 list flex justify-start items-center'>
-            <li>
-              <label>
-                <input type="checkbox" className='mr-4' />
-                Apples
-              </label>
+      
+        {/* Groceries to Buy */}
+        <div className='bg-gray-200 mx-8 p-4 h-fit mt-8'>
+          <h1 className='text-xl text-center font-extrabold'>Groceries we need</h1>
+          <ul className=' mt-6 list flex justify-start items-center'>
+            <li className='text-3xl'>
+              Apples
+              <button>
+              <AiFillCheckCircle size={30} color='green'/>
+              </button>
+              <button>
+              <VscClose size={30} color='red' />
+              </button>
             </li>
           </ul>
 
         </div>
-
-        {/* Right Column - Groceries Bought Already */}
-        <div className='bg-green-200 p-4 mr-4 h-fit'>
-          <h1 className='text-xl text-center'>Groceries we bought</h1>
-        </div>
-      </div>
-
-
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+        {/* Right Column - Groceries Bought Already
+        <div className='bg-green-200 p-4 mr-4 h-fit'>
+          <h1 className='text-xl text-center font-extrabold'>Groceries we have</h1>
+
+          <ul className=' mt-10 list flex justify-start items-center'>
+            <li>
+              <label>
+                <input type="checkbox" className='mr-4' />
+                Bananas
+              </label>
+            </li>
+          </ul>
+
+
+        </div> */}
