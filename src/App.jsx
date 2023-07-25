b4279079a5eb1bd0ca28784c7fa1a01a03483351
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-07-24 22:33:23 -0500

    added click function for completed Item, functions perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,19 @@ function App() {
     })
 
     setNewItem("")
-
   }
 
+  function handleCompleteItem(id) {
+    setTodos((currentTodos) =>
+      currentTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+  
 
   return (
     <>
-
       <h1 className='flex text-4xl font-bold justify-center mt-[6vh] mb-[3vh]'>
         Brick's Grocery List
       </h1>
@@ -43,35 +49,35 @@ function App() {
         </input>
         <button className=' btn'>Add</button>
       </form>
-
       
         {/* Groceries to Buy */}
         <div className='bg-gray-200 mx-8 p-4 h-fit mt-8'>
-          <h1 className='text-xl text-center font-extrabold'>Groceries we need</h1>
+        <h1 className='text-xl text-center font-extrabold'>
+          Groceries we need
+        </h1>
 
-          <ul className=' mt-6 list flex flex-col justify-start items-center'>
-            {todos.map(todo => {
-              return <li key={todo.id}
-              className='text-3xl'>
+        <ul className='mt-6 list flex flex-col justify-start items-center'>
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className={`text-4xl ${todo.completed ? 'completed' : ''}`}
+            >
+              <button onClick={() => handleCompleteItem(todo.id)}>
+                <AiFillCheckCircle size={30} color='green' />
+              </button>
               {todo.title}
               <button>
-              <AiFillCheckCircle size={30} color='green'/>
-              </button>
-              <button>
-              <VscClose size={30} color='red' />
+                <VscClose size={30} color='red' />
               </button>
             </li>
-            })}
-            
-          </ul>
-
-        </div>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
 
 
 
