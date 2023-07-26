60d0c65174e0a16308eabad0a865efc6b37ea87e
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-07-26 15:48:13 -0500

    styled list, adjusted buttons on list, cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { AiFillCheckCircle } from 'react-icons/ai'
-import { VscClose } from 'react-icons/vsc'
-import InputForm from './components/InputForm'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { AiFillCheckCircle } from 'react-icons/ai';
+import { VscClose } from 'react-icons/vsc';
+import InputForm from './components/InputForm';
 
 function App() {
-
   const [todos, setTodos] = useState(() => {
     const storedTools = localStorage.getItem('todos');
     return storedTools ? JSON.parse(storedTools) : [];
-  })
+  });
 
-  {/* useEffect will monitor changes to 'todos' and save it to local storage */ }
-
+  // useEffect will monitor changes to 'todos' and save it to local storage
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
@@ -26,9 +24,9 @@ function App() {
   }
 
   function handleDeleteItem(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
+    setTodos((currentTodos) =>
+      currentTodos.filter((todo) => todo.id !== id)
+    );
   }
 
   return (
@@ -39,7 +37,7 @@ function App() {
 
       <InputForm setTodos={setTodos} />
 
-      <div className='bg-gray-200 mx-8 p-4 h-fit mt-8'>
+      <div className='bg-gray-200 mx-2 p-4 h-fit mt-8'>
         <h1 className='text-xl text-center font-extrabold'>
           Groceries we need
         </h1>
@@ -47,8 +45,9 @@ function App() {
           {todos.map((todo) => (
             <li
               key={todo.id}
-              className={`text-4xl flex items-center justify-between ${todo.completed ? 'completed' : ''
-                }`}
+              className={`text-2xl flex items-center justify-between ${
+                todo.completed ? 'completed' : ''
+              }`}
             >
               <div className='flex items-center'>
                 <input
