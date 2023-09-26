20e6dea6effd8f9b9d7e8275004f74cd5672137c
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-09-26 02:18:10 -0500

    added grid colors

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -22,7 +22,7 @@ function GridLayout({ savedLists, setSavedLists }) {
     <p className='mt-8 text-2xl text-red-600 text-center'>*Under Construction*</p>
     <div className="grid grid-cols-2 gap-4 p-4 mt-16 mb-8 cursor-not-allowed">
       {savedLists.map((list, index) => (
-        <div key={index} className="card text-black relative">
+        <div key={index} className="card text-black bg-gray-200 relative">
           <button
             className='absolute top-0 right-0 px-[5px]'
             onClick={() => handleDeleteList(index)}>
